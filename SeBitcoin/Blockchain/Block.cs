07c4868f324bed35243814d0ba7addcc340ab9af
Bakker,Sebas S <sebasbakker8@hotmail.com>
2022-02-22 14:53:55 +0100

    Added basic encryption logic

---
+++ SeBitcoin/Blockchain/Block.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeBitcoin.Blockchain
+{
+    class Block
+    {
+        private string previousHash;
+        private string blockDataHashed;
+        private EncryptionLogic eLog;
+
+        public Block(string preHash, string data, int dif)
+        {
+            eLog = new EncryptionLogic(dif);
+            previousHash = preHash;
+            blockDataHashed = eLog.MineBlock(preHash, data);
+        }
+
+        public string getPreviousHash()
+        {
+            return previousHash;
+        }
+
+        public string getBlockDataHashed()
+        {
+            return blockDataHashed;
+        }
+
+        public string getBlockDataInString()
+        {
+            string dif = eLog.getDifficulty().ToString();
+            string tries = eLog.getNonce().ToString();
+            return " with previous hash: " + this.getPreviousHash() + " and current hash: " + this.getBlockDataHashed() + ", difficulty: " + dif + " tries " + tries;
+        }
+    }
+}
---
+++ SeBitcoin/Blockchain/EncryptionLogic.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeBitcoin.Blockchain
+{
+    class EncryptionLogic
+    {
+        //Nonce is used to mine block
+        private int nonce = 0;
+        private bool cracked = false;
+
+        //How many zeroes a block needs to start with to be approved
+        private int difficulty;
+
+        public EncryptionLogic(int dif)
+        {
+            difficulty = dif;
+        }
+
+        public string HashData(string data)
+        {
+            return "We'll do this later";
+        }
+
+        public string MineBlock(string preBlockHash, string currData)
+        {
+            //Prefix string creates a new string with amount of zeroes equal to the difficulty level.
+            string prefixString = new string(new char[difficulty]).Replace('\0', '0');
+
+            while (!cracked)
+            {
+                if (this.HashData(preBlockHash + currData + nonce.ToString()).Substring(0, difficulty).Equals(prefixString))
+                {
+                    cracked = true;
+                }
+                else
+                {
+                    nonce++;
+                }
+            }
+            Console.WriteLine(preBlockHash);
+            Console.WriteLine(currData);
+            Console.WriteLine(nonce);
+
+            return this.HashData(preBlockHash + currData + nonce.ToString());
+        }
+
+        public int getNonce()
+        {
+            return nonce;
+        }
+
+        public int getDifficulty()
+        {
+            return difficulty;
+        }
+    }
+}
